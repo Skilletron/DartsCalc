68896135ae2bcdfeff06da41f1ef6533b1cd1ff7
Konstantin Volobuev <62390419+Skilletron@users.noreply.github.com>
2020-10-22 16:54:15 +0300

    Final Darts

---
+++ Calculator Darts/Assets/scripts/AddPlayer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddPlayer : MonoBehaviour
+{
+    public GameObject NeedName;
+    public GameObject[] playeradd = new GameObject[6];
+    public GameObject[] buttonplus = new GameObject[6];
+    public Text[] score = new Text[6];
+    public Text[] name = new Text[6] ;
+    public InputField field;
+    public Player_1 player;
+    public ButtonCalc btn;
+    public int i;
+    private void Awake()
+    {
+        i = -1;
+    }
+    public void Update()
+    {
+        if (i < 0)
+        {
+            btn.attempts = 0;
+            btn.Score = 0;
+            player.round_1[0].SetActive(false);
+            player.round_1[1].SetActive(false);
+            player.round_1[2].SetActive(false);
+            player.round_2[0].SetActive(false);
+            player.round_2[1].SetActive(false);
+            player.round_2[2].SetActive(false);
+            player.round_3[0].SetActive(false);
+            player.round_3[1].SetActive(false);
+            player.round_3[2].SetActive(false);
+            player.round_4[0].SetActive(false);
+            player.round_4[1].SetActive(false);
+            player.round_4[2].SetActive(false);
+            player.round_5[0].SetActive(false);
+            player.round_5[1].SetActive(false);
+            player.round_5[2].SetActive(false);
+        }
+       
+    }
+    public void PlayerClickAdd()
+    {
+        TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default,true,false,true); ;
+        NeedName.SetActive(true);
+        i++;
+        playeradd[i].SetActive(true);
+        buttonplus[i].SetActive(false);
+
+    }
+    public void Next()
+    {
+        NeedName.SetActive(false);
+        name[i].text = field.text;
+        if (name[i].text == "")
+        {
+            name[i].text = "PLAYER" + (i+1).ToString("");
+        }
+        
+        score[i].text = "0";
+    }
+
+}
---
+++ Calculator Darts/Assets/scripts/Winner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class Winner : MonoBehaviour
+{
+    public Player_1 player;
+    public TMP_Text text;
+    public AddPlayer addPlayer;
+    public GameObject win;
+    public void Update()
+    {
+
+        for(int i = 0; i<=4 ; i++)
+        {
+            if (player.Score[i] <= 0)
+            {
+                text.text = addPlayer.name[i].text;
+                player.Score[i] = 0;
+                win.SetActive(true);
+
+            }
+       
+        }
+   }
+    public void Button_Restart()
+    {
+        win.SetActive(false);
+        
+    }
+    public void Button_Exit()
+    {
+        Application.Quit();
+    }
+}
